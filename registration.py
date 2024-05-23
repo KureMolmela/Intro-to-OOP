43a7882b0acf5143be529d95cc434ba5d6c0a10b
KureMolmela <molmela.kure-yusuf@binghamuni.edu.ng>
2024-05-23 09:46:54 +0100

    class examples

---
+++ registration.py
@@ -0,0 +1,12 @@
+from user import User
+
+jane = User("Bingham University", "Karu, Nasarawa")
+jane.show()
+
+# user = User()
+# user.register("james", "111", "abc","abc")
+
+email = input("Enter Email: ")
+
+password = input("Enter Password: ")
+print(user.login(email, password))
---
+++ user.py
@@ -0,0 +1,18 @@
+class User:
+
+    def __init__(self, schoolName, schoolAddress):
+        self.schoolName = schoolName
+        self.schoolAddress = schoolAddress
+
+    def show(self):
+        print("School Name: ", self.schoolName, "School Address: ", self.schoolAddress)
+
+    def register(self, name, phone, email, password):
+        return name
+
+    def login(self, email, password):
+        if email == "test@mail.com" and password == "abc":
+            return True
+        else:
+            return False
+
